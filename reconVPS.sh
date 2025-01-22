49d69e0d6ba46026ff68bfcf3038b2274eafba44
hunThub <49031710+hunThubSpace@users.noreply.github.com>
2025-01-22 11:21:22 +0330

    Update reconVPS.sh

--- reconVPS.sh
+++ reconVPS.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
 # Define color codes
-RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
 RESET='\033[0m'
 
 echo -e "${GREEN}[+] Update & Upgrade VPS Packages${RESET}"
